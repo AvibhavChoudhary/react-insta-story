b102058dbd76be0333f4a24e2377464be83a222b
Avibhav Choudhary <avibhavchoudhary@Avibhavs-MacBook-Pro.local>
2025-04-05 00:39:37 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// src/App.tsx
+import React from "react";
+import "./App.css"; // Import global styles
+
+const App: React.FC = () => {
+  return <div className="app">Hello World</div>;
+};
+
+export default App;
